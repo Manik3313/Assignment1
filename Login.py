906f283cb4c5ef521f136eda605f83fca38ee71f
Manik3313 <68765331+Manik3313@users.noreply.github.com>
2020-12-01 19:32:02 +0530

    Update Login.py

--- Login.py
+++ Login.py
@@ -4,6 +4,6 @@ class Test_login(Base_class):
         self.driver.find_element_by_xpath("//div/input[@name='email']").send_keys("rajeshpanwar1717@gmail.com")
         self.driver.find_element_by_xpath("//div/input[@name='password']").send_keys("password")
         self.driver.find_element_by_xpath("//button[@type='submit']").click()
-        print("hello")
+        print("hello123")
 
 
